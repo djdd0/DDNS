38169fcddef5458cb1f7c76fd786fdcba441eb57
djdd <787838245@qq.com>
2024-05-01 16:36:11 +0800

    fix get properties bug.

--- src/main/java/djdd/ddns/GetProperties.java
+++ src/main/java/djdd/ddns/GetProperties.java
@@ -50,7 +50,7 @@ public class GetProperties {
 
     public static void readProperties() throws IOException {
         Properties prop = new Properties();
-        Reader in = new InputStreamReader(Files.newInputStream(Paths.get("properties.txt")), StandardCharsets.UTF_8);
+        Reader in = new InputStreamReader(Files.newInputStream(Paths.get(System.getProperty("user.dir")+"/properties.txt")), StandardCharsets.UTF_8);
         prop.load(in);
         domainName = prop.getProperty("domainName");
         RR = prop.getProperty("RR");
